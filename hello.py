ccc58f4882d05da9ed12af028fedf5e01429057c
Kateryna Unytska <catherine.unitskaya@gmail.com>
2021-07-07 21:30:48 +0300

    commit is the best

--- hello.py
+++ hello.py
@@ -1,1 +1,30 @@
 print('Hello, world!')
+
+for x in range(0,6):
+    print('healthy')
+for y in range(0,4):
+    print('happy')
+print(list(range(10,20)))
+for x in range(0,5):
+    print('life %s'% x)
+кількість_монет = 200
+print(кількість_монет)
+літр=200
+print(літр)
+a=20
+b=10
+c=3
+print(a+b*365-c*52)
+c=2
+print(a+b*365-c*52)
+a='''djfjb
+jrgbjrgr'''
+print(a)
+f='dst:"jfn \`lglrf"'
+print(f)
+d=1000
+text="I received %s messages"
+print(text % d)
+numbers = '''що число %s say to %s
+!!!'''
+print(numbers % (0,8))
